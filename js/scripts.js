74c8de5740a81308321a7f271eba42c1a8c7b1a1
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2022-08-24 14:24:37 -0700

    correct js code to run user interface

--- js/scripts.js
+++ js/scripts.js
@@ -19,16 +19,14 @@ function divide(num1, num2) {
 // user interface logic 
 function handleCalculation(event) {
   event.preventDefault();
-// ADD  CODE TO GET AND PROCESS FORM VALUES
-}
+
 const num1 = parseInt(document.querySelector("input#input1").value);
 const num2 = parseInt(document.querySelector("input#input2").value);
 const operator = document.querySelector("input[name='operator']:checked").value;
 console.log("Number 1: " + num1);
 console.log("Number 2: " + num2);
 console.log("Operator:", operator);
-
-// window.alert("The result of your numbers is " + add(num1, num2) + ". " + "The remainder of your numbers is " + subtract(num1, num2) + ". " + "The multiplication of your numbers is " + multiply(num1, num2) + ". " + "The division of your numbers is " + divide(num1, num2) + ".");
+}
 
 window.addEventListener("load", function() {
   const form = document.getElementById("calculator");
