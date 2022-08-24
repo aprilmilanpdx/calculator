3c761ba5046d1f8f088e828e9c03948528a60333
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2022-08-24 14:15:04 -0700

    add event listeners to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
 // business logic
 
-function add(number1, number2) {
-  return number1 + number2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(number1,number2) {
-  return number1 - number2;
+function subtract(num1,num2) {
+  return num1 - num2;
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
 // user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function handleCalculation(event) {
+  event.preventDefault();
+// ADD  CODE TO GET AND PROCESS FORM VALUES
+}
+const num1 = parseInt(document.querySelector("input#input1").value);
+const num2 = parseInt(document.querySelector("input#input2").value);
+const operator = document.querySelector("input[name='operator']:checked").value;
+console.log("Number 1: " + num1);
+console.log("Number 2: " + num2);
+console.log("Operator:", operator);
 
-window.alert("The result of your numbers is " + add(number1, number2) + ". " + "The remainder of your numbers is " + subtract(number1, number2) + ". " + "The multiplication of your numbers is " + multiply(number1, number2) + ". " + "The division of your numbers is " + divide(number1, number2) + ".");
+// window.alert("The result of your numbers is " + add(num1, num2) + ". " + "The remainder of your numbers is " + subtract(num1, num2) + ". " + "The multiplication of your numbers is " + multiply(num1, num2) + ". " + "The division of your numbers is " + divide(num1, num2) + ".");
 
-//window.alert("The remainder of your numbers is " + subtract(number1, number2));
-
-//window.alert("The multiplication of your numbers is " + multiply(number1, number2));
-
-//window.alert("The division of your numbers is " + divide(number1, number2));
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
